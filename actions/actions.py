0f47967e39368777cc25949b8e520eef89cca569
ntttrung <trung.ntt1210@gmail.com>
2022-01-13 20:36:14 +0700

    .

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,99 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from unidecode import unidecode
+from rasa_sdk.events import EventType, SlotSet
+import requests
+from email.mime.text import MIMEText
+import json
+import smtplib
+from email.mime.multipart import MIMEMultipart
+
+
+
+class ActionSubmit(Action):
+    def name(self) -> Text:
+        return "action_submit"
+
+    def run(
+        self,
+        dispatcher,
+        tracker: Tracker,
+        domain: "DomainDict"
+    ) -> List[Dict[Text, Any]]:
+
+        SendEmail(
+            tracker.get_slot("email"),tracker.get_slot("message")
+        )
+
+        dispatcher.utter_message("Cảm ơn bạn đã gửi câu hỏi cho tổ tư vấn, tư vấn viên sẽ xem xét và trả lời câu hỏi của bạn sớm nhất có thể.")
+        return []
+
+def SendEmail(email,message):
+    fromaddr = 'evnchatbot@gmail.com'
+    msg = MIMEMultipart()
+    toaddr = 'evnchatbot@gmail.com'
+
+    msg['To'] = "evnchatbot@gmail.com"
+    msg['Subject'] = "EVN - QUESTION FROM: " + email
+    body = message
+    msg.attach(MIMEText(body, 'plain'))
+    
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+
+    s.starttls()
+
+    try:
+       # password here
+        s.login(fromaddr, "icommvietnam")
+        text = msg.as_string()
+        s.sendmail(fromaddr, toaddr, text)
+    except:
+        print("Error")
+    finally:
+        s.quit()
+
+
+
+class Specifier(Action):
+
+    def name(self) -> Text:
+        return 'action_specifier'
+
+    def run(self,dispatcher: CollectingDispatcher,tracker: Tracker,domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        tra_cuu = ['tra cuu']  
+        bac_1 = ['bac 1']
+        bac_2 = ['bac 2']
+        bac_3 = ['bac 3']
+        phan_nan = ['cao the', 'doc quyen', 'toi te', 'ngu dot', 'ngu', 'khong nhan duoc thong bao', 'doc ton', 'qua cao']
+
+        id_user = tracker.sender_id
+        f = open("user_database.txt", "a")
+        f.write(id_user+"\n")
+        f.close()
+
+
+        user_input = unidecode(tracker.latest_message.get('text')).lower()
+        print(user_input)
+        intent = tracker.latest_message['intent'].get('name')
+        ogintent = intent
+
+        if any(map(user_input.__contains__, tra_cuu)):
+           intent = "utter_tra_cuu_thong_tin"
+        elif any(map(user_input.__contains__, phan_nan)):
+           intent = "utter_xin_loi"
+        else :
+           intent = "utter_" + ogintent
+        if(user_input.find('https') != -1) or ((user_input.find('hhtps') != -1)):
+           intent = "utter_emotes"
+        print(tracker.latest_message)   
+        print(intent)
+        try:
+         dispatcher.utter_message(template=intent)
+        except:
+         dispatcher.utter_message(template="utter_"+ogintent)
+        return []
+
---
+++ data/preprocesser.py
@@ -0,0 +1,167 @@
+from rasa.nlu.components import Component
+from rasa.nlu import utils
+from rasa.nlu.model import Metadata
+from unidecode import unidecode
+from pyvi import ViUtils
+from typing import Any, Dict, List, Text
+from rasa.shared.nlu.training_data.message import Message
+from rasa.nlu.constants import TOKENS_NAMES
+from rasa.core.agent import Agent
+import os
+import pickle
+import re
+import nltk
+import requests
+from nltk.tokenize.treebank import TreebankWordDetokenizer
+from rasa.utils.endpoints import EndpointConfig 
+import json
+import time
+
+
+class VietnamesePreprocesser(Component):
+
+    name = "VietnamesePreprocesser"
+    provides = ["entities"]
+    requires = ["message"]
+
+    def __init__(self, component_config=None):
+        print("Preloading accent model" )
+        #nltk.download('punkt')
+        # tai cai model tai day : https://github.com/vapormusic/mfe_models/releases/
+        #model_dir = "./fasttext/"
+        #with open(os.path.join(model_dir, 'kneserney_1st_ngram_model.pkl'), 'rb') as fin:
+        #     self.model_loaded = pickle.load(fin)
+         
+        super().__init__(component_config)
+
+    def train(self, training_data, cfg, **kwargs):
+        """Not needed, because the the model is pretrained"""
+        pass
+        
+
+          
+    def process(self, message : Message, **kwargs):
+        
+            
+        def isEnglish(s):
+            x = re.search("[ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéTêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ]", s)
+            if x:
+              return False
+            else:
+              return True  
+        def TelexConvert(s):
+         #  x = re.search("[ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéTêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ]", s)
+         words = s.split()
+         evn = ['evn']
+         newwords = []
+         for word in words:
+          tempword = word
+          if tempword not in evn:
+            if tempword.find("aw") != -1:
+                tempword = tempword.replace("aw", "ă")
+            elif tempword.find("aa") != -1:
+                tempword = tempword.replace("aa", "â")
+            elif tempword.find("dd") != -1:
+                tempword = tempword.replace("dd", "đ")
+            elif tempword.find("oo") != -1:
+                tempword = tempword.replace("oo", "ô")
+            elif tempword.find("ow") != -1:
+                tempword = tempword.replace("ow", "ơ")
+            elif tempword.find("ee") != -1:
+                tempword = tempword.replace("ee", "ê")
+            elif tempword.find("uw") != -1:
+                tempword = tempword.replace("uw", "ư")
+            elif tempword.find("w") != -1:
+                tempword = tempword.replace("w", "ư")
+                
+            if tempword.find("tr") == -1 & tempword[1:].find("r") != -1:
+                tempword = tempword.replace("r", "")
+            elif tempword.find("tr") != -1 & tempword[2:].find("r") != -1:
+                tempword = tempword.replace("tr", "zzcazp")
+                tempword = tempword.replace("r", "")
+                tempword = tempword.replace("zzcazp", "tr")
+            elif tempword[1:].find("s") != -1:
+                tempword = tempword.replace("s", "")
+            elif tempword[1:].find("f") != -1:
+                tempword = tempword.replace("f", "")
+            elif tempword[2:].find("x") != -1:
+                tempword = tempword.replace("x", "")
+            elif tempword.find("j") != -1:
+                tempword = tempword.replace("j", "")
+          newwords.append(tempword)
+        #  return unidecode(" ".join(newwords))     
+          return newwords
+         
+        def StopWordRemover(s):
+             words = s.split()
+             stopwords = ["cung","cũng","chắc","chac","ntn","đc","uh","vậy","vay","thì","nhỉ","thế","ừ","ko","nào","nao", "lam the nao", "làm thế nào", "ê", 'e',"khong", "the", "bạn", "em", "anh", "chị", "hỏi", "Em", "tôi", 'có', 'mình', '?', "ạ"]
+             newwords = []
+             for word in words:
+                # print(word)
+                if word in stopwords:
+                #    print("gone")
+                   newwords.append("")
+                else:
+                   newwords.append(word)
+             return " ".join(newwords)
+        def convert_num(s):
+            dict = {'0':'không', '1':'một', '2':'hai', '3':'ba',
+                    '4': 'bốn', '5':'năm', '6': 'sáu', '7':'bảy',
+                    '8': 'tám', '9':'chín'}
+            charac = ''
+            for i in s:
+                if i in dict.keys():
+                    i = dict[i]
+                charac += i
+            return charac
+             
+        try:
+         text_data1 = ((str(message.get("text"))).lower()) 
+         time.sleep(10)
+         text_data2 = ((str(message.get("text"))).lower()) 
+         if text_data1 == text_data2:
+             text_data = text_data1
+         else: 
+             text_data = text_data1 + ' ' + text_data2
+         my_data = {}
+         my_data['text'] = text_data
+         r = requests.post(url="http://10.9.3.239:1212/spelling_correction", data=json.dumps(my_data))
+         text_data = r.text.replace('"',"")
+         
+        #  print(text_data)
+
+         if message.get("text") != None :
+           fillers = ["em muốn hỏi", "thêm thông tin", "thay co cho em hoi","thầy cô cho em hỏi","anh chị cho em hỏi","anh chi cho em hoi", "cho mình hỏi", "cho minh hoi","cho em hỏi","cho em hoi",
+                        "em hỏi","page ơi", "tôi muốn hỏi", "tôi muốn tìm hiểu", "toi muon tim hieu", "với ạ", "của trường", "tư vấn giúp em",  "cho em", "ra sao"
+                        "giúp mình", "tư vấn giúp mình", "cho hỏi", "xin hỏi", "mình muốn tìm hiểu", "thì sao", "tôi muốn hỏi", "kinh tế quốc dân", "kinh te quoc dan", "trường mình",
+                        "trường tư vấn cho em", "cho em hỏi là", "mình có nhu cầu", "ktqd", "chatbot", '2020', '2021', "em muốn tìm hiểu", "muốn tìm hiểu", "bao nhiêu ạ", "có đúng không", "không biết", 'thầy cô']
+        #    text_data = convert_num(text_data)
+        #    text_data = StopWordRemover((str(message.get("text"))).lower())
+           for filler in fillers:
+             try:
+              text_data = text_data.replace(filler,"")
+              pass
+             except:
+              print("no")           
+           text_data = StopWordRemover(text_data) 
+        #    if(isEnglish(text_data)):
+        #      new_message = TelexConvert(text_data)
+        #    else:
+        #      new_message = text_data      
+        #    new_message = TelexConvert(text_data)      
+        #    print("lmao2:" + new_message)
+           new_message = text_data
+           new_message = " ".join(new_message.split())
+        #    print(new_message)
+           message.set("text", new_message)
+        except :
+         pass
+          
+
+    def persist(self, file_name, model_dir):
+        """Pass because a pre-trained model is already persisted"""
+        pass
+        
+    
+	
+             
